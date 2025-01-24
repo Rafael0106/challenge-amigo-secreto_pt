35f0d598b494cddcfa651e510a4dfd115c3c8377
Rafael Sobral Coutinho <raafaelsobrall@gmail.com>
2025-01-24 15:41:44 -0300

    add function adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let = amigos = [];
+
+function adicionarAmigo(){
+    nomeAmigo = document.getElementById('amigo').value;
+    if (nomeAmigo == ''){
+        alert('Por favor, insira um nome!')
+    } else {
+        amigos.push(nomeAmigo);
+        nomeAmigo = document.querySelector('input');
+        nomeAmigo.value = '';
+        console.log(amigos);
+    }
+}
